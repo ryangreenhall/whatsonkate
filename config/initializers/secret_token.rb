ae381a740c8d6396054a591b6fb4343f8366ec1d
Ryan Greenhall <ryan.greenhall@gmail.com>
2011-11-18 14:24:16 +0000

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatsonkate::Application.config.secret_token = '8926320eca408edbc0cb47d2e6c2a6399dc0465928a8463434ef8140c936fa05bfcffbd3e7c56e931b6671d03026757decd18e58a9a2a9cec5c2e655986ed0aa'
